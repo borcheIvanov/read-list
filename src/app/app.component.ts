99dd0275b61bdf235ddd395f361c2460b76758ee
borcheIvanov <borche.ivanov@outlook.com>
2025-03-29 20:56:24 +0100

    feat: add new pages and improve style (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  login() {
+    // TODO: Implement login functionality
+    console.log('Login clicked');
+  }
+}
